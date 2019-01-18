224472dba0f32ad43ac54078a3eccd2f79e228f1
Alberto López <colkard96@gmail.com>
2019-01-19 00:11:54 +0100

    Working book day

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
 const request = require('request');
 const moment = require('moment');
 var _ = require('lodash');
+
+var Password = require('./Password');
+var aCookies;
+var loginAH = (sMail, sPW, fnSuccess) => {
   request({
-    url: `https://crossfitboxcastelldefels.aimharder.com/api/bookings?day=20190118&familyId=&box=9283&_=${moment().valueOf()}`,
+    url: `https://aimharder.com/login`,
     jar: true,
-    method: "GET",
+    method: "POST",
     headers: {
-      "Cookie": `_ga=GA1.2.985420368.1529072355; __stripe_mid=28396597-08e3-451e-8ada-0d0ad287ac56; __stripe_mid=28396597-08e3-451e-8ada-0d0ad287ac56; _gid=GA1.2.1752464807.1547842153; _fbp=fb.1.1547842153300.930074486; PHPSESSID=f0i18flt96qkolc3kbhl5dp7p1; crisp-client%2Fsession%2Fdf0c7671-9c6c-407e-8f6e-2b319d29f188=session_37ca8427-3292-4d97-81e1-9446533923a9; amhrdrauth=23688%7C1555621390%7Cee33eef06c1aa2b4358c7cff6bf6f51b; __stripe_sid=12e3b1b3-99bc-46d8-b59d-9eeb9c1ebcb8; AWSELB=F571F9EF16E8876E7FD628E559CADE048064DC36AF575857E35B35B2B490265A43D739FA80EE5DF68ADEAE623FDF1E44D288342E4CA03269AB87CB85842277624208CCEF00; _gat=1`
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    form: {
+      "mail": sMail,
+      "pw": sPW,
+      "login": "Iniciar sesión"
     }
   })
   .on('response', function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    aCookies = res.headers["set-cookie"];
+    fnSuccess();
+  })
+};
+
+var getBookings = (sDate, fnSuccess) => {
+  request({
+    url: `https://crossfitboxcastelldefels.aimharder.com/api/bookings?day=${sDate}&familyId=&box=9283&_=${moment().valueOf()}`,
+    jar: true,
+    method: "GET",
+    headers: {
+      "Cookie": aCookies.join("; ")
+    }
+  })
+  .on('response', function(res) {
+    console.log('get booking STATUS: ' + res.statusCode);
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
       var oData = JSON.parse(chunk);
-      var oClass = _.find(oData.bookings, element => element.time === "18:30 - 19:30");
-      console.log(oClass);
+      console.log(oData);
+      fnSuccess(oData.bookings);
     });
   })
+}
+
+var bookClass = (sClassID, sBookDay, fnSuccess) => {
+  request({
+    url: `https://crossfitboxcastelldefels.aimharder.com/api/book`,
+    jar: true,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Cookie": aCookies.join("; ")
+    },
+    form: {
+      "id": sClassID,
+      "day": sBookDay,
+      "insist": "0",
+      "familyId": ""
+    }
+  })
+  .on('response', fnSuccess)
+}
+
+var aDaysToBook = [
+  {
+    1: "21:00 - 22:00"
+  },
+  {
+    2: "21:00 - 22:00"
+  },
+  {
+    3: "20:30 - 21:30"
+  },
+  {
+    4: "20:30 - 21:30"
+  },
+  {
+    5: "18:30 - 19:30"
+  },
+]
+
+loginAH("colkard96@gmail.com", Constants.password, function(res) {
+  //TODO logic book days
+  var sBookDay = moment().add("days", 1).format("YYYYMMDD");
+  var date = moment();
+  var dow = date.day();
+  console.log(dow);
+  getBookings(sBookDay, aAvailableClasses => {
+      var oClass = _.find(aAvailableClasses, element => element.time === "12:00 - 13:30");
+      if (!oClass) return;
+      if (!oClass.bookState) bookClass(oClass.id, sBookDay, () => console.log("Dia reservado"));
+      //bookClass(oClass.id, sBookDay, () => console.log("Dia reservado"));
+  })
+})
