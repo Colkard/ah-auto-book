d8be62e4980b58a5cff72e2af460e09104896002
López P, Alberto <alberto.lopez.p@accenture.com>
2019-01-21 15:38:49 +0100

    Beautified

--- Config.js
+++ Config.js
@@ -1,28 +1,33 @@
 module.exports = {
-  api_url: 'https://crossfitboxcastelldefels.aimharder.com/api',
-  login_url: 'https://aimharder.com/login',
-  iCountDaysToBook: 3,
-  iBoxID: "9283",
-  aDaysToBook: [
-    {
-      Day: 1,
-      Time: "21:00 - 22:00"
-    },
-    {
-      Day: 2,
-      Time: "21:00 - 22:00"
-    },
-    {
-      Day: 3,
-      Time: "20:30 - 21:30"
-    },
-    {
-      Day: 4,
-      Time: "20:30 - 21:30"
-    },
-    {
-      Day: 5,
-      Time: "18:30 - 19:30"
-    },
-  ]
+    api_url: 'https://crossfitboxcastelldefels.aimharder.com/api',
+    login_url: 'https://aimharder.com/login',
+    iCountDaysToBook: 3,
+    iBoxID: "9283",
+    aDaysToBook: [
+        {
+            Day: 1,
+            Time: "21:00 - 22:00",
+            ClassName: "WOD"
+        },
+        {
+            Day: 2,
+            Time: "21:00 - 22:00",
+            ClassName: "Barbell Conditioning"
+        },
+        {
+            Day: 3,
+            Time: "20:30 - 21:30",
+            ClassName: "WOD"
+        },
+        {
+            Day: 4,
+            Time: "20:30 - 21:30",
+            ClassName: "Weightlifting"
+        },
+        {
+            Day: 5,
+            Time: "18:30 - 19:30",
+            ClassName: "WOD"
+        },
+    ]
 };
--- index.js
+++ index.js
@@ -6,85 +6,84 @@ var config = require('./Config');
 
 var aCookies;
 var loginAH = fnSuccess => {
-  request({
-    url: config.login_url,
-    jar: true,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    form: {
-      "mail": process.env.email,
-      "pw": process.env.pass,
-      "login": "Iniciar sesión"
-    }
-  })
-  .on('response', function(res) {
-    aCookies = res.headers["set-cookie"];
-    fnSuccess();
-  })
+    request({
+        url: config.login_url,
+        jar: true,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        form: {
+            "mail": process.env.email,
+            "pw": process.env.pass,
+            "login": "Iniciar sesión"
+        }
+    })
+        .on('response', function (res) {
+            aCookies = res.headers["set-cookie"];
+            fnSuccess();
+        })
 };
 
 var getBookings = (sDate, fnSuccess, fnError) => {
-  request({
-    url: `${config.api_url}/bookings?day=${sDate}&familyId=&box=${config.iBoxID}&_=${moment().valueOf()}`,
-    jar: true,
-    method: "GET",
-    headers: {
-      "Cookie": aCookies.join("; ")
-    }
-  })
-  .on('response', function(res) {
-    console.log(`${sDate} get booking STATUS: ${res.statusCode}`);
-    res.setEncoding('utf8');
+    request({
+        url: `${config.api_url}/bookings?day=${sDate}&familyId=&box=${config.iBoxID}&_=${moment().valueOf()}`,
+        jar: true,
+        method: "GET",
+        headers: {
+            "Cookie": aCookies.join("; ")
+        }
+    })
+        .on('response', function (res) {
+            console.log(`${sDate} get booking STATUS: ${res.statusCode}`);
+            res.setEncoding('utf8');
 
-    var body = '';
-    res.on('data', function (chunk) {
-      body += chunk;
-    });
+            var body = '';
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
 
-    res.on('end', function () {
-      var oData = JSON.parse(body);
-      if (oData.bookings && oData.bookings.length) fnSuccess(oData.bookings);
-      else if (oData.bookings && !oData.bookings.length) fnError("No classes available");
-    });
-  })
+            res.on('end', function () {
+                var oData = JSON.parse(body);
+                if (oData.bookings && oData.bookings.length) fnSuccess(oData.bookings);
+                else if (oData.bookings && !oData.bookings.length) fnError("No classes available");
+            });
+        })
 }
 
 var bookClass = (sClassID, sBookDay, fnSuccess) => {
-  request({
-    url: `${config.api_url}/book`,
-    jar: true,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Cookie": aCookies.join("; ")
-    },
-    form: {
-      "id": sClassID,
-      "day": sBookDay,
-      "insist": "0",
-      "familyId": ""
-    }
-  })
-  .on('response', fnSuccess)
-}
+    request({
+        url: `${config.api_url}/book`,
+        jar: true,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": aCookies.join("; ")
+        },
+        form: {
+            "id": sClassID,
+            "day": sBookDay,
+            "insist": "0",
+            "familyId": ""
+        }
+    }).on('response', fnSuccess)
+};
 
-  console.log(moment().format("HH:mm:ss"))
-  loginAH((res) => {
+console.log(moment().format("HH:mm:ss"))
+loginAH(res => {
     for (var i = 0; i <= config.iCountDaysToBook; i++) {
-      var oBookDay = moment().add(i, "days");
-      var iForwardDay = oBookDay.day();
-      var sBookDay = oBookDay.format("YYYYMMDD");
-      ((oBookDay, iForwardDay, sBookDay) => {
-        getBookings(sBookDay, aAvailableClasses => {
-            var oTimeToBook = _.find(config.aDaysToBook, element => element.Day === iForwardDay);
-            if (!oTimeToBook) return;
-            var oClass = _.find(aAvailableClasses, element => element.time === oTimeToBook.Time);
-            if (!oClass) return;
-            if (!oClass.bookState) bookClass(oClass.id, sBookDay, () => console.log(`Dia ${oBookDay.format("DD-MM-YYYY")} reservado durante ${oTimeToBook.Time}.`));
-        }, console.error)
-      })(oBookDay, iForwardDay, sBookDay)
+        var oBookDay = moment().add(i, "days");
+        var iForwardDay = oBookDay.day();
+        var sBookDay = oBookDay.format("YYYYMMDD");
+        ((oBookDay, iForwardDay, sBookDay) => {
+            getBookings(sBookDay, aAvailableClasses => {
+                var oTimeToBook = _.find(config.aDaysToBook, element => element.Day === iForwardDay);
+                if (!oTimeToBook) return;
+                var oClass = _.find(aAvailableClasses, element => element.time === oTimeToBook.Time && element.className === oTimeToBook.ClassName);
+                if (!oClass) return;
+                if (!oClass.bookState) bookClass(oClass.id, sBookDay, () => console.log(`Dia ${oBookDay.format("DD-MM-YYYY")} reservado durante ${oTimeToBook.Time}.`));
+            }, console.error)
+        })(oBookDay, iForwardDay, sBookDay)
     }
-  })
+});
 
